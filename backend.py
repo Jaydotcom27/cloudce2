c9c32b3c24de237c1e794849a6c21ed165a113c6
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-07-29 20:02:06 -0400

    Switched to port 5000

--- backend.py
+++ backend.py
@@ -47,4 +47,4 @@ def delete_item(item):
     return jsonify({"message": "Item deleted successfully"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
